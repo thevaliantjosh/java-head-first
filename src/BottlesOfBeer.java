93a6f3751410c04c0c384e90f84ca31557d61ad5
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-09 13:14:09 -0600

    Bottles of Beer Method Refactored

--- src/BottlesOfBeer.java
+++ src/BottlesOfBeer.java
@@ -1,28 +1,35 @@
 public class BottlesOfBeer {
-    public static String bottlesOfBeer(int numberOfBeers){
+    public static void bottlesOfBeer(int numberOfBeers){
         String bottlesOfBeer = "Bottles of Beer";
         String onTheWall = "On The Wall";
         String takeOneDown = "Take one Down, pass it around!";
-        int originalBeerAmount = numberOfBeers;
         while(numberOfBeers > 0){
 
-            System.out.printf("%d %s %s, %d %s.%n %s%n", numberOfBeers, bottlesOfBeer, onTheWall, numberOfBeers, bottlesOfBeer, takeOneDown);
+            if(numberOfBeers > 1){
+                System.out.printf("%d %s %s, %d %s.%n %s%n", numberOfBeers, bottlesOfBeer, onTheWall, numberOfBeers, bottlesOfBeer, takeOneDown);
+            } else {
+                System.out.printf("%d %s %s, %d %s.%n %s%n", numberOfBeers, "Bottle of Beer", onTheWall, numberOfBeers, "Bottle of Beer", takeOneDown);
+            }
             numberOfBeers--;
-            if (numberOfBeers > 0){
+            if (numberOfBeers > 1){
 
                 System.out.printf("%d %s %s!%n", numberOfBeers, bottlesOfBeer, onTheWall);
                 System.out.println("----------------------------------------------------");
 
-            } else {
+
+            } else if (numberOfBeers == 1){
+                System.out.printf("%d Bottle of Beer %s%n", numberOfBeers, onTheWall);
+                System.out.println("----------------------------------------------------");
+
+            }  else {
                 System.out.println("No More bottles of beer on the Waaaaaallll!!");
             }
-        }
-        return "No more bottles of beer on the wall!";
-    }
+        }//End of While loop
+    }//End of Bottles of Beer Method
     public static void main(String[] args) {
-        //Make a program that will sing the "bottles of bear song" counting down from 10
+        //Make a program that will sing the "bottles of bear song" counting down from any number
 
-        bottlesOfBeer(10);
+        bottlesOfBeer(99);
 
     }
 }
