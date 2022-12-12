5dc52e17dbad5342559448e17e70ca2d8a556d75
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-12-12 13:33:35 -0600

    Echo, and EchoTestDrive

---
+++ src/pool_puzzle/Echo.java
@@ -0,0 +1,58 @@
+package pool_puzzle;
+
+public class Echo {
+
+    //Instance Variables
+
+    private int count;
+
+    private int numberOfOs;
+
+    private String theOs = "o";
+
+    //Getters and Setters
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getNumberOfOs() {
+        return numberOfOs;
+    }
+
+    public void setNumberOfOs(int numberOfOs) {
+        this.numberOfOs = numberOfOs;
+    }
+
+    public String getTheOs() {
+        return theOs;
+    }
+
+    public void setTheOs(String theOs) {
+        this.theOs = theOs;
+    }
+
+    //Constructors
+
+    public Echo(){
+
+    }
+
+    public Echo(int count, String theOs)
+
+
+    //Methods
+
+
+
+
+
+    public void hello(){
+        System.out.println("hello");
+    }
+
+}
---
+++ src/pool_puzzle/EchoTestDrive.java
@@ -0,0 +1,10 @@
+package pool_puzzle;
+
+public class EchoTestDrive {
+
+    //Write a program that will say Hello, but repeat the Hello based on the number you provide, and add o's to the end of hello base on that count, the amount of o's should decrease bases on that repeated number
+
+
+    Echo echo = new Echo();
+
+}
